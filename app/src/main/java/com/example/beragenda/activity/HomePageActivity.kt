99abf338134b86134bbb74fd735319f77d3620bb
daveradin753 <daveputraradin@gmail.com>
2021-06-29 18:26:02 +0700

    signin function

---
+++ app/src/main/java/com/example/beragenda/activity/HomePageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.beragenda.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.beragenda.R
+
+class HomePageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+    }
+}
--- app/src/main/java/com/example/beragenda/fragment/SignInFragment.kt
+++ app/src/main/java/com/example/beragenda/fragment/SignInFragment.kt
@@ -1,19 +1,32 @@
 package com.example.beragenda.fragment
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.FragmentManager
 import com.example.beragenda.R
+import com.example.beragenda.activity.HomePageActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
 import org.w3c.dom.Text
 
 class SignInFragment : Fragment() {
 
     private lateinit var tvToSignUp: TextView
     private lateinit var tvForgotYourPasswordSignIn: TextView
+    private lateinit var etSignInEmailUser: EditText
+    private lateinit var etSignInPasswordUser: EditText
+    private lateinit var btnSignin: Button
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -26,10 +39,13 @@ class SignInFragment : Fragment() {
 
         tvToSignUp = view.findViewById(R.id.tvToSignUp)
         tvForgotYourPasswordSignIn = view.findViewById(R.id.tvForgotYourPasswordSignIn)
+        etSignInEmailUser = view.findViewById(R.id.etSigninEmailUser)
+        etSignInPasswordUser = view.findViewById(R.id.etSigninPasswordUser)
+        btnSignin = view.findViewById(R.id.btnSignIn)
 
         val SignUpFragment = SignUpFragment()
         val ForgotPasswordFragment = ForgotPasswordFragment()
-        val fragmentManager = fragmentManager
+        val fragmentManager = childFragmentManager
 
         tvToSignUp.setOnClickListener {
             fragmentManager?.beginTransaction()?.apply {
@@ -47,7 +63,42 @@ class SignInFragment : Fragment() {
             }
         }
 
+        btnSignin.setOnClickListener {
+            val email: String = etSignInEmailUser.text.toString()
+            val password: String = etSignInPasswordUser.text.toString()
 
+            if (TextUtils.isEmpty(email)) {
+                etSignInEmailUser.error = "Email is required!"
+                return@setOnClickListener
+            }
+            if (TextUtils.isEmpty(password)) {
+                etSignInPasswordUser.error = "Password is required!"
+                return@setOnClickListener
+            }
+            if (password.length < 8){
+                etSignInPasswordUser.error = "Password must be more than 8 characters!"
+                return@setOnClickListener
+            }
+            signIn(email, password)
+        }
+
+
+    }
+
+    private fun signIn(email: String, password: String) {
+        auth = FirebaseAuth.getInstance()
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if(it.isSuccessful) {
+                    Toast.makeText(this.context,"Login Success", Toast.LENGTH_SHORT).show()
+                    Log.d("LOGIN", "signInWithEmail:success")
+                    val intent = Intent(this.context, HomePageActivity::class.java)
+                    startActivity(intent)
+                }else {
+                    Toast.makeText(this.context,"Login failed", Toast.LENGTH_SHORT).show()
+                    Log.d("LOGIN", "signInWithEmail:failed")
+                }
+            }
     }
 
     companion object {
