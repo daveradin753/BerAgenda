ee1b8a3679aeed60daa00024efe41e225e5fdf12
philliptc <phill.tc05@gmail.com>
2021-06-29 19:21:33 +0700

    ForgotPasswordFragment done

--- app/src/main/java/com/example/beragenda/fragment/ForgotPasswordFragment.kt
+++ app/src/main/java/com/example/beragenda/fragment/ForgotPasswordFragment.kt
@@ -1,13 +1,26 @@
 package com.example.beragenda.fragment
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import com.example.beragenda.R
+import com.google.firebase.auth.FirebaseAuth
 
 class ForgotPasswordFragment : Fragment() {
+
+    private lateinit var tvLoginForgotYourPassword : TextView
+    private lateinit var btnSendForgotYourPassword : Button
+    private lateinit var etForgotYourPasswordEmailUser : EditText
+    private lateinit var auth : FirebaseAuth
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -18,6 +31,45 @@ class ForgotPasswordFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        tvLoginForgotYourPassword = view.findViewById(R.id.tvLoginForgotYourPassword)
+        btnSendForgotYourPassword = view.findViewById(R.id.btnSendForgotYourPassword)
+        etForgotYourPasswordEmailUser = view.findViewById(R.id.etForgotYourPasswordEmailUser)
+        auth = FirebaseAuth.getInstance()
+
+        val SignInFragment = SignInFragment()
+        val ResendForgotPasswordFragment = ResendForgotPasswordFragment()
+        val fragmentManager = fragmentManager
+
+        tvLoginForgotYourPassword.setOnClickListener {
+            fragmentManager?.beginTransaction()?.apply {
+                replace(R.id.flSigninSignup, SignInFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        btnSendForgotYourPassword.setOnClickListener {
+            val email : String = etForgotYourPasswordEmailUser.text.toString()
+            if (TextUtils.isEmpty(email)) {
+                etForgotYourPasswordEmailUser.error = "Email is required!"
+                return@setOnClickListener
+            }
+            auth.sendPasswordResetEmail(etForgotYourPasswordEmailUser.text.toString()).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Log.d("FORGOT PASS", "Email sent.")
+                    val bundle = Bundle()
+                    bundle.putString("forgotpasswordemail", email)
+                    val ResendForgotPasswordFragment = ResendForgotPasswordFragment()
+                    ResendForgotPasswordFragment.arguments = bundle
+                    fragmentManager?.beginTransaction()?.apply {
+                        replace(R.id.flSigninSignup, ResendForgotPasswordFragment)
+                        addToBackStack(null)
+                        commit()
+                    }
+                }
+
+            }
+        }
 
     }
 
--- app/src/main/java/com/example/beragenda/fragment/ResendForgotPasswordFragment.kt
+++ app/src/main/java/com/example/beragenda/fragment/ResendForgotPasswordFragment.kt
@@ -1,20 +1,56 @@
 package com.example.beragenda.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import com.example.beragenda.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 
 
 class ResendForgotPasswordFragment : Fragment() {
 
+    private lateinit var btnToLoginFromResendPass : Button
+    private lateinit var tvResendEmail : TextView
+    private lateinit var auth : FirebaseAuth
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_resend_forgot_password, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnToLoginFromResendPass = view.findViewById(R.id.btnToLoginFromResendPass)
+        tvResendEmail = view.findViewById(R.id.tvResendEmail)
+        auth = FirebaseAuth.getInstance()
+
+        val SignInFragment = SignInFragment()
+        val email = arguments?.getString("forgotpasswordemail")
+
+        btnToLoginFromResendPass.setOnClickListener {
+            fragmentManager?.beginTransaction()?.apply {
+                replace(R.id.flSigninSignup, SignInFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+        tvResendEmail.setOnClickListener {
+            auth.sendPasswordResetEmail(email).addOnCompleteListener {
+                if (it.isSuccessful){
+                    Log.d("FORGOT PASS", "Email sent.")
+                    Toast.makeText(this.context, "Email has been sent!", Toast.LENGTH_SHORT)
+                }
+            }
+        }
     }
 
     companion object {
