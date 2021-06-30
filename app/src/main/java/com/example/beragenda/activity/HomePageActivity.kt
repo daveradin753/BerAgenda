be1ea2ee2060cefd81f3caa7e51236700f05772f
daveradin753 <daveputraradin@gmail.com>
2021-06-30 15:25:32 +0700

    homepage layout

--- app/src/main/java/com/example/beragenda/activity/HomePageActivity.kt
+++ app/src/main/java/com/example/beragenda/activity/HomePageActivity.kt
@@ -12,7 +12,6 @@ class HomePageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
-
         setSupportActionBar(findViewById(R.id.homeToolbar))
     }
 
