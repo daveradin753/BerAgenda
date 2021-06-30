6c3351a002fe886f8bd7b01395a2bd478de8dca7
daveradin753 <daveputraradin@gmail.com>
2021-06-30 15:22:34 +0700

    homepage layout

--- app/src/main/java/com/example/beragenda/activity/HomePageActivity.kt
+++ app/src/main/java/com/example/beragenda/activity/HomePageActivity.kt
@@ -2,11 +2,24 @@ package com.example.beragenda.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Toolbar
 import com.example.beragenda.R
 
 class HomePageActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+
+        setSupportActionBar(findViewById(R.id.homeToolbar))
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.actionbar_board, menu)
+        return true
+    }
+
 }
