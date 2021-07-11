09192d601723a05fc5e7e82cf49d8b0f08023e1d
Reynard Euginio Tjokro <reynardets@gmail.com>
2021-07-11 20:10:57 +0700

    add language setting

--- app/src/main/java/com/example/beragenda/activity/SettingActivity.kt
+++ app/src/main/java/com/example/beragenda/activity/SettingActivity.kt
@@ -1,9 +1,13 @@
 package com.example.beragenda.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.beragenda.R
 
 class SettingActivity : AppCompatActivity() {
@@ -11,8 +15,16 @@ class SettingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
 
-        setSupportActionBar(findViewById(R.id.settingPageToolbar))
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        val language = findViewById<ConstraintLayout>(R.id.firstButton)
+        language.setOnClickListener {
+            val mIntent = Intent(Settings.ACTION_LOCALE_SETTINGS)
+            startActivity(mIntent)
+        }
+
+        val imageView = findViewById<ImageView>(R.id.back)
+        imageView.setOnClickListener {
+            finish()
+        }
 
     }
 
