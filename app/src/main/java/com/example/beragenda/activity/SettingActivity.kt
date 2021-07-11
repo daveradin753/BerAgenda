2eed008952d4f7c5c21b8e33378cf39bb5f1c898
Reynard Euginio Tjokro <reynardets@gmail.com>
2021-07-11 20:40:20 +0700

    darkMode
    notSure yet

--- app/src/main/java/com/example/beragenda/activity/SettingActivity.kt
+++ app/src/main/java/com/example/beragenda/activity/SettingActivity.kt
@@ -1,5 +1,6 @@
 package com.example.beragenda.activity
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,13 +8,24 @@ import android.provider.Settings
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.beragenda.R
 
 class SettingActivity : AppCompatActivity() {
+
+    companion object {
+        private const val IS_CHECK = "is_check"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
+
+        val SWITCH_ON = "switch_on"
+        val sharedPreferences = getSharedPreferences("alarmClick", Context.MODE_PRIVATE)
+        val boolean = sharedPreferences.getBoolean(SWITCH_ON, false)
 
         val language = findViewById<ConstraintLayout>(R.id.firstButton)
         language.setOnClickListener {
@@ -26,12 +38,26 @@ class SettingActivity : AppCompatActivity() {
             finish()
         }
 
-    }
+        val darkSwitch = findViewById<SwitchCompat>(R.id.darkSwitch)
+        darkSwitch.isChecked = boolean
 
+        darkSwitch.setOnCheckedChangeListener { _, isChecked ->
+            val editor = sharedPreferences.edit()
+            if (isChecked) {
+                editor.apply {
+                    putBoolean(SWITCH_ON, true)
+                }.apply()
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                delegate.applyDayNight()
+            } else {
+                editor.apply {
+                    putBoolean(SWITCH_ON, false)
+                }.apply()
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                delegate.applyDayNight()
+            }
+        }
 
-    override fun onSupportNavigateUp(): Boolean {
-        onBackPressed()
-        return true
     }
 
 }
